17995ea22550a45a2d0c9e4d4725c3d72b17589c
patryk <patrykwojcik2@gmail.com>
2018-03-04 01:05:47 +0100

    przedostatnie zadanie. wychodzi na to, że jest ok, bo test zdaje

--- lab1/multiplicationtable/MultiplicationTable.cpp
+++ lab1/multiplicationtable/MultiplicationTable.cpp
@@ -14,9 +14,14 @@ void wyswietl(int tab[][10])
 {
     for(int i=0;i<10;i++)
     {
-        for (int j=0;j<10;j++)
-        {
+        for (int j=0;j<10;j++) {
             cout << tab[i][j];
+            if (tab[i][j] < 10) {
+                cout << "  ";
+            } else {
+                cout << " ";
+            }
         }
+        cout<<endl;
     }
 }
