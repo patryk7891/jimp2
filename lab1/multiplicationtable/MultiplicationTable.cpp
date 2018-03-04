ce574d0b7512ab88528bfe63f5089335c8b50888
patryk <patrykwojcik2@gmail.com>
2018-03-04 02:21:27 +0100

    macierz mnozenia

--- lab1/multiplicationtable/MultiplicationTable.cpp
+++ lab1/multiplicationtable/MultiplicationTable.cpp
@@ -4,7 +4,7 @@ void MultiplicationTable(int tab[][10])
 {
     for(int i=0;i<10;i++)
     {
-        for (int j=0;j<10;j++)
+        for (int j=0; j<10; j++)
         {
             tab[i][j]=(i+1)*(j+1);
         }
