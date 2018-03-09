44878e6395fe4a5ae75481ee31c4b6571b2d9c05
patryk <patrykwojcik2@gmail.com>
2018-03-09 21:20:13 +0100

    zrobione zadanie forwardlist

--- lab2/simpleforwardlist/SimpleForwardList.cpp
+++ lab2/simpleforwardlist/SimpleForwardList.cpp
@@ -3,3 +3,54 @@
 //
 
 #include "SimpleForwardList.h"
+ForwardList *CreateNode(int value)
+{
+    ForwardList *pierwszy,*nowy;
+    pierwszy=nullptr;
+    nowy=new ForwardList;
+    nowy->value=value;
+    nowy->next=pierwszy;
+    pierwszy=nowy;
+    return pierwszy;
+
+
+}
+void DestroyList(ForwardList *list)
+{
+    ForwardList *nastepny;
+    while(list)
+    {
+        nastepny=list->next;
+        delete list;
+        list=nastepny;
+    }
+}
+ForwardList *PushFront(ForwardList *list, int value)
+{
+    if (list)
+    {
+        ForwardList *nowy;
+        nowy=new ForwardList;
+        nowy->value=value;
+        nowy->next=list;
+        list=nowy;
+        return list;
+    }
+
+
+
+}
+void Append(ForwardList *list, ForwardList *tail)
+{
+    if (list)
+    {
+        while(list->next)
+        {
+            list=list->next;
+        }
+        list->next=tail;
+    }
+    else list=tail;
+
+
+}
--- lab2/simpleforwardlist/main.cpp
+++ lab2/simpleforwardlist/main.cpp
@@ -1,4 +1,4 @@
 //
 // Created by mikolaj on 05.03.18.
 //
-
+#include "SimpleForwardList.h"
