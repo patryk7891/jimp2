5f640d137eadf4f24e4b92870147dbbb77e8b650
Mikolaj <mikiogarek@gmail.com>
2018-03-12 22:36:31 +0100

    Poprawiam Array2d

--- lab2/array2d/Array2d.h
+++ lab2/array2d/Array2d.h
@@ -9,6 +9,7 @@ using namespace std;
 #include <iostream>
 
 int **Array2D(int n_rows, int n_columns);
+void FillArray2D(int n_rows, int n_columns, int **tab);
 void DeleteArray2D(int **array, int n_rows, int n_columns);
 int **NewArray2D(int n_rows, int n_columns);
 #endif //JIMP_EXERCISES_ARRAY2D_H
