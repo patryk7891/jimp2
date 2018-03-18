7e066955b73bdb521403abebcb9e399e08f79e4f
patryk <patrykwojcik2@gmail.com>
2018-03-18 16:31:21 +0100

    Napisano kod dla step1 i step2 przy użyciu dwóch elementów w strukturze: tablicy dla hasha oraz stringa dla adresu url

--- lab3/tinyurl/TinyUrl.cpp
+++ lab3/tinyurl/TinyUrl.cpp
@@ -1,5 +1,34 @@
-//
-// Created by mikolaj on 12.03.18.
-//
-
 #include "TinyUrl.h"
+
+namespace tinyurl {
+    std::unique_ptr<TinyUrlCodec> Init() {
+        std::unique_ptr<TinyUrlCodec> first_object = std::make_unique<TinyUrlCodec>();
+        first_object->hash = {'0','0','0','0','0','0'};
+        return first_object;
+    }
+
+    void NextHash(std::array<char, 6> *state) {
+        std::string alfabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        unsigned long indeks, i=5;
+        while(state->at(i)=='z'){
+            state->at(i)='0';
+            i--;
+        }
+        indeks=alfabet.find(state->at(i));
+        state->at(i)=alfabet.at(indeks+1);
+    }
+
+    std::string Encode(const std::string &url, std::unique_ptr<TinyUrlCodec> *codec) {
+        NextHash(&codec->get()->hash);
+        std::string hash;
+        for (auto letter: codec->get()->hash)
+            hash += letter;
+        codec->get()->m=url;
+        return hash;
+    }
+
+    std::string Decode(const std::unique_ptr<TinyUrlCodec> &codec, const std::string &hash) {
+        std::string url=codec->m;
+        return url;
+    }
+}
--- lab3/tinyurl/main.cpp
+++ lab3/tinyurl/main.cpp
@@ -1,4 +1,39 @@
-//
-// Created by mikolaj on 12.03.18.
-//
+#include "TinyUrl.h"
+#include <iostream>
+#include <vector>
+int main(){
 
+
+    std::string code = "000000"; //oo
+    std::array<char, 6> tab;
+
+    for (int i = 0; i < 6; ++i)
+        (tab)[i] = code[i];
+
+    std::vector<std::string> tinyUrlBunchOfUrlsTestData
+            {"https://google.com", "https://wikipedia.org", "https://regex101.com/",
+             "http://www.regular-expressions.info/recurse.html#balanced", "https://xkcd.com/1755/", "https://xkcd.com/1758/",
+             "https://xkcd.com/1775/", "http://stackoverflow.com/questions/29397066/c-regexp-to-parsing-nested-struct",
+             "http://www.regular-expressions.info/boost.html",
+             "https://phys.org/news/2016-09-discovery-quasars-ancient-universe.html",
+             "https://phys.org/news/2017-03-discovery-bevy-quasars-boost-efforts.html",
+             "https://www.youtube.com/watch?v=FkCT_LV9Syk", "http://ai.ia.agh.edu.pl/wiki/pl:dydaktyka:jimp2:2017:labs:pamiec2",
+             "https://leetcode.com/problemset/algorithms/#", "https://en.wikipedia.org/wiki/List_of_Unicode_characters",
+             "http://unicode.org/reports/tr15/", "https://reviewable.io/", "https://github.com/integrations",
+             "https://github.com/integrations/travis-ci", "https://github.com/integrations/sonarqube",
+             "http://www.talkenglish.com/vocabulary/top-2000-vocabulary.aspx", "https://www.topcoder.com",
+             "https://pl.wikipedia.org/wiki/Filtr_Kalmana", "https://www.youtube.com/watch?v=2pzxEmh0gYw",
+             "http://geofl.ge/#!/page_saxelmdzvanelo", "https://www.youtube.com/watch?v=K83YZYcpExc",
+             "https://www.memrise.com/", "https://www.duolingo.com/"
+            };
+    std::cout<<"4"<<std::endl<<std::endl;
+    std::unique_ptr<tinyurl::TinyUrlCodec> codec = tinyurl::Init();
+
+    for (auto &url : tinyUrlBunchOfUrlsTestData) {
+
+
+        std::string hash=Encode(url,&codec);
+        std::cout<<tinyurl::Decode(codec,hash)<<std::endl;
+    }
+    return 0;
+}
