619602f55c3ce238f8be1beb249b3d7af706b7f9
Mikolaj <mikiogarek@gmail.com>
2018-04-16 21:53:24 +0200

    liczba-test

---
+++ lab7/liczba/Liczba.cpp
@@ -0,0 +1,5 @@
+//
+// Created by mikolaj on 16.04.18.
+//
+
+#include "Liczba.h"
---
+++ lab7/liczba/Liczba.h
@@ -0,0 +1,14 @@
+//
+// Created by mikolaj on 16.04.18.
+//
+
+#ifndef JIMP_EXERCISES_LICZBA_H
+#define JIMP_EXERCISES_LICZBA_H
+
+
+class Liczba {
+
+};
+
+
+#endif //JIMP_EXERCISES_LICZBA_H
