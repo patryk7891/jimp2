3751b2738524eff2d82760c3f5d6cda5213cdf36
Mikolaj <mikiogarek@gmail.com>
2018-04-24 00:31:04 +0200

    pesel

--- lab8/pesel/main.cpp
+++ lab8/pesel/main.cpp
@@ -7,5 +7,21 @@
 
 int main()
 {
+
+        const char* str = "00000000000";
+        try{
+            academia::Pesel a(str);
+        }catch(academia::InvalidPeselChecksum b){
+            std::cout << "zly checksum" << std::endl;
+        }catch(academia::InvalidPeselLength c) {
+            std::cout << "zla dlugosc" << std::endl;
+        }
+        catch(academia::InvalidPeselCharacter d) {
+            std::cout << "litery w peselu... troche slabo" << std::endl;
+        }
+        catch(...){
+            std::cout << "Blad " << std::endl;
+        }
+
     return 0;
 }
