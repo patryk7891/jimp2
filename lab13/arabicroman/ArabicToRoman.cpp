e93c7d224dc2d85d475607c648851aef44dcb54c
Mikolaj <mikiogarek@gmail.com>
2018-06-05 08:36:04 +0200

    araby

---
+++ lab13/arabicroman/ArabicToRoman.cpp
@@ -0,0 +1,5 @@
+//
+// Created by mikolaj on 05.06.18.
+//
+
+#include "ArabicToRoman.h"
---
+++ lab13/arabicroman/ArabicToRoman.h
@@ -0,0 +1,14 @@
+//
+// Created by mikolaj on 05.06.18.
+//
+
+#ifndef JIMP_EXERCISES_ARABICTOROMAN_H
+#define JIMP_EXERCISES_ARABICTOROMAN_H
+
+
+class ArabicToRoman {
+
+};
+
+
+#endif //JIMP_EXERCISES_ARABICTOROMAN_H
